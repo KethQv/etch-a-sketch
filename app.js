962885d4e51cc0120ac77d0afc89ec97797f3993
Kenneth Quintero <kennethgqv@gmail.com>
2021-05-03 14:53:10 -0600

    this fix the multi flip and implement random colors

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const grid = document.querySelector('.grid');
 
+const pixelLength = '175px';
+
 for (let i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.classList = 'pixel';
-    div.style.width  = '100px';
-    div.style.height = '100px';
-    grid.appendChild(div);
+    let pixelContainer = document.createElement('div');
+    pixelContainer.classList = 'pixel-container';
+    let pixel = document.createElement('div');
+    pixel.classList = 'pixel';
+    pixel.style.width  = pixelLength;
+    pixel.style.height = pixelLength;
+    grid.appendChild(pixelContainer);
+    pixelContainer.appendChild(pixel);
 }
 
 let colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
 let pixels = document.querySelectorAll('.pixel');
+let pixelsContainers = document.querySelectorAll('.pixel-container');
 
+function randColor() {
+    return Math.floor(Math.random() * 256);
+}
 for (let i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener('mouseenter', (e) => {
-        pixels[i].style.background = colors[Math.floor(Math.random() * 5)];
+    pixelsContainers[i].addEventListener('mouseenter', (e) => {
+        //pixels[i].style.backgroundColor = colors[Math.floor(Math.random() * 5)];
+        pixels[i].style.backgroundColor = `rgba(${randColor()}, ${randColor()}, ${randColor()}, 1)`;
+        pixels[i].classList.toggle('pixel--flip');
     });
 }
